cd281bce2983c9cec7d33c3cd848a53321375835
fizevizl <dnruban@gmail.com>
2024-11-15 19:00:24 +0100

    nabrosok_1

---
+++ bot.py
@@ -0,0 +1,17 @@
+import telebot
+import config
+
+bot = telebot.TeleBot(config.token)
+
+bot.message_handler(commands=['start'])
+def start_message(message, callback_data=None):
+    markup = telebot.types.InlineKeyboardMarkup()
+    markup.add(telebot.types.InlineKeyboardButton(text='Студент', callback_data='s'))
+    markup.add(telebot.types.InlineKeyboardButton(text='Викладач', callback_data='v'))
+    bot.send_message(message.chat.id, text="Bітаю! Хто Ви?", reply_markup=markup)
+
+@bot.message_handler(content_types=['text'])
+def lalala(message):
+    bot.send_message(message.chat.id, message.text) 
+    
+bot.polling(none_stop=True)
